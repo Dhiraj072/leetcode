3efa3301db646c14ed9bf51e488a56080c112f79
Dhiraj <dhirajbhatt072@gmail.com>
2018-08-03 22:17:11 +0800

    Refactor tests for ArrayRotator, extract out an abstract class for easier testing for future implementations

--- src/test/java/com/github/dhiraj072/leetcode/solutions/arrays/SimpleArrayRotatorTest.java
+++ src/test/java/com/github/dhiraj072/leetcode/solutions/arrays/SimpleArrayRotatorTest.java
@@ -1,64 +1,14 @@
 package com.github.dhiraj072.leetcode.solutions.arrays;
 
-import static org.junit.Assert.assertArrayEquals;
 
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
+public class SimpleArrayRotatorTest extends ArrayRotatorTestBase {
 
-public class SimpleArrayRotatorTest {
+  private SimpleArrayRotator arrayRotator;
 
-  SimpleArrayRotator arrayRotator;
-
-  @Before
-  public void init() {
+  @Override
+  protected SimpleArrayRotator createRotator() {
 
     arrayRotator = new SimpleArrayRotator();
-  }
-
-  @Test
-  public void rotateStepInMiddle() {
-
-    int[] arr = { 1, 2, 3, 4, 5, 6, 7};
-    int[] expectedArr = { 5, 6, 7, 1, 2, 3, 4 };
-    int[] result = arrayRotator.rotate(arr, 3);
-    assertArrayEquals(expectedArr, result);
-  }
-
-  @Test
-  public void rotateStepOne() {
-
-    int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
-    int[] expectedArr = { 7, 1, 2, 3, 4, 5, 6 };
-    int[] result = arrayRotator.rotate(arr, 1);
-    assertArrayEquals(expectedArr, result);
-  }
-
-  @Test
-  public void rotateStepZero() {
-
-    int[] arr = { 1, 2, 3, 4, 5, 6, 7};
-    int[] expectedArr = { 1, 2, 3, 4, 5, 6, 7};
-    int[] result = arrayRotator.rotate(arr, 0);
-    assertArrayEquals(expectedArr, result);
-  }
-
-  @Test
-  public void rotateStepEqualsArrayLength() {
-
-    int[] arr = { 1, 2, 3, 4, 5, 6, 7};
-    int[] expectedArr = { 1, 2, 3, 4, 5, 6, 7};
-    int[] result = arrayRotator.rotate(arr, 7);
-    assertArrayEquals(expectedArr, result);
-  }
-
-  // TODO FIXME gives out of bounds expception
-  @Ignore
-  public void rotateSingleElementArray() {
-
-    int[] arr = { 1 };
-    int[] expectedArr = { 1 };
-    int[] result = arrayRotator.rotate(arr, 7);
-    assertArrayEquals(expectedArr, result);
+    return arrayRotator;
   }
 }
