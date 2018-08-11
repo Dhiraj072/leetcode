1a733e362c073998354d086391f751f079892527
Dhiraj <dhirajbhatt072@gmail.com>
2018-08-11 23:43:35 +0800

    Refactor out common test helper for binary trees to another class as it's needed for other solutions
    Add cucumber package in build.gradle that I missed in previous commits

--- src/test/java/com/github/dhiraj072/leetcode/solutions/trees/BinaryTreeMaxDepthCalculatorTest.java
+++ src/test/java/com/github/dhiraj072/leetcode/solutions/trees/BinaryTreeMaxDepthCalculatorTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class BinaryTreeMaxDepthCalculatorTest {
+public class BinaryTreeMaxDepthCalculatorTest extends BinaryTreeTestBase {
 
   private static final Logger LOGGER =
       LoggerFactory.getLogger(BinaryTreeMaxDepthCalculatorTest.class);
@@ -55,46 +55,5 @@ public class BinaryTreeMaxDepthCalculatorTest {
     root = generateBinaryTree(initArr);
     assertEquals("Incorrect max-depth", 3,
         binaryTreeMaxDepthCalculator.maxDepth(root));
-  }
-
-  /**
-   * Test helper - Generates a binary tree
-   * @param arr - Array of numbers to generate binary tree
-   * @return root node of the binary tree
-   *
-   *  -1 is interpreted as null node
-   * e.g.
-   * Given array [3,9,20,-1,-1,15,7], the generated binary tree is
-   *     3
-   *   /   \
-   *   9   20
-   *      /  \
-   *     15   7
-   *
-   */
-  private BinaryTreeNode generateBinaryTree(int[] arr) {
-
-    if (arr.length == 0) {
-
-      return null;
-    }
-    return buildNode(arr, 0, 1, 2);
-  }
-
-  private BinaryTreeNode buildNode(int[] arr, int nodeIndex, int leftNodeIndex,
-      int rightNodeIndex) {
-
-    if (nodeIndex >= arr.length || arr[nodeIndex] == -1) {
-
-      return null;
-    }
-
-    BinaryTreeNode node = new BinaryTreeNode(arr[nodeIndex]);
-    node.left = buildNode(arr, leftNodeIndex, 2 * (leftNodeIndex + 1) - 1,
-        2 * (leftNodeIndex + 1));
-    node.right = buildNode(arr, rightNodeIndex, 2 * (rightNodeIndex + 1) - 1,
-        2 * (rightNodeIndex + 1));
-
-    return node;
   }
 }
---
+++ src/test/java/com/github/dhiraj072/leetcode/solutions/trees/BinaryTreeTestBase.java
@@ -0,0 +1,45 @@
+package com.github.dhiraj072.leetcode.solutions.trees;
+
+abstract class BinaryTreeTestBase {
+
+  /**
+   * Test helper - Generates a binary tree
+   * @param arr - Array of numbers to generate binary tree
+   * @return root node of the binary tree
+   *
+   *  -1 is interpreted as null node
+   * e.g.
+   * Given array [3,9,20,-1,-1,15,7], the generated binary tree is
+   *     3
+   *   /   \
+   *   9   20
+   *      /  \
+   *     15   7
+   *
+   */
+  BinaryTreeNode generateBinaryTree(int[] arr) {
+
+    if (arr.length == 0) {
+
+      return null;
+    }
+    return buildNode(arr, 0, 1, 2);
+  }
+
+  private BinaryTreeNode buildNode(int[] arr, int nodeIndex, int leftNodeIndex,
+      int rightNodeIndex) {
+
+    if (nodeIndex >= arr.length || arr[nodeIndex] == -1) {
+
+      return null;
+    }
+
+    BinaryTreeNode node = new BinaryTreeNode(arr[nodeIndex]);
+    node.left = buildNode(arr, leftNodeIndex, 2 * (leftNodeIndex + 1) - 1,
+        2 * (leftNodeIndex + 1));
+    node.right = buildNode(arr, rightNodeIndex, 2 * (rightNodeIndex + 1) - 1,
+        2 * (rightNodeIndex + 1));
+
+    return node;
+  }
+}
