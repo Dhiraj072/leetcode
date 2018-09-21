d7498301b0d7a9b4029bda55e3ce3661c23b7186
Dhiraj <dhirajbhatt072@gmail.com>
2018-09-21 23:58:49 +0800

    Initial class and test for Power of Three - yet to implement

---
+++ src/main/java/com/github/dhiraj072/leetcode/solutions/math/PowerOfThree.java
@@ -0,0 +1,9 @@
+package com.github.dhiraj072.leetcode.solutions.math;
+
+public class PowerOfThree {
+
+  public static boolean isPowerOfThree(int num) {
+
+    return false;
+  }
+}
---
+++ src/test/java/com/github/dhiraj072/leetcode/solutions/math/PowerOfThreeTest.java
@@ -0,0 +1,16 @@
+package com.github.dhiraj072.leetcode.solutions.math;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+
+class PowerOfThreeTest {
+
+  @Disabled
+  @Test
+  public void testShouldReturnTrue() {
+
+    assertTrue(PowerOfThree.isPowerOfThree(27));
+  }
+}
