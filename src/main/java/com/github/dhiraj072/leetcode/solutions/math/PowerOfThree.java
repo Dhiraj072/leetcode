a47ef25d902defd9737691208463119ecdbe48a8
Dhiraj <dhirajbhatt072@gmail.com>
2018-09-22 22:34:05 +0800

    Simple loop solution for Power of Three

--- src/main/java/com/github/dhiraj072/leetcode/solutions/math/PowerOfThree.java
+++ src/main/java/com/github/dhiraj072/leetcode/solutions/math/PowerOfThree.java
@@ -2,8 +2,20 @@ package com.github.dhiraj072.leetcode.solutions.math;
 
 public class PowerOfThree {
 
-  public static boolean isPowerOfThree(int num) {
+  public static boolean isPowerOfThree(int n) {
 
-    return false;
+    if (n <= 0) {
+
+      return false;
+    }
+    while (n > 1) {
+
+      if (n % 3 != 0) {
+
+        return false;
+      }
+      n = n / 3;
+    }
+    return true;
   }
 }
--- src/test/java/com/github/dhiraj072/leetcode/solutions/math/PowerOfThreeTest.java
+++ src/test/java/com/github/dhiraj072/leetcode/solutions/math/PowerOfThreeTest.java
@@ -2,15 +2,49 @@ package com.github.dhiraj072.leetcode.solutions.math;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 class PowerOfThreeTest {
 
-  @Disabled
   @Test
-  public void testShouldReturnTrue() {
+  void test27ShouldReturnTrue() {
 
     assertTrue(PowerOfThree.isPowerOfThree(27));
+  }
+
+  @Test
+  void test50ShouldReturnFalse() {
+
+    assertFalse(PowerOfThree.isPowerOfThree(50));
+  }
+
+  @Test
+  void test3ShouldReturnTrue() {
+
+    assertTrue(PowerOfThree.isPowerOfThree(3));
+  }
+
+  @Test
+  void test9ShouldReturnTrue() {
+
+    assertTrue(PowerOfThree.isPowerOfThree(9));
+  }
+
+  @Test
+  void test36ShouldReturnFalse() {
+
+    assertFalse(PowerOfThree.isPowerOfThree(36));
+  }
+
+  @Test
+  void test0ShouldReturnFalse() {
+
+    assertFalse(PowerOfThree.isPowerOfThree(0));
+  }
+
+  @Test
+  void test1ShouldReturnTrue() {
+
+    assertTrue(PowerOfThree.isPowerOfThree(1));
   }
 }
