4864932efbd26cfdc80bfa2b544a0d5a8c94886d
Dhiraj <dhirajbhatt072@gmail.com>
2018-10-02 21:00:00 +0800

    More test cases for merge sorted

--- src/test/java/com/github/dhiraj072/leetcode/solutions/arrays/MergeSortedArrayTest.java
+++ src/test/java/com/github/dhiraj072/leetcode/solutions/arrays/MergeSortedArrayTest.java
@@ -7,11 +7,47 @@ import org.junit.jupiter.api.Test;
 class MergeSortedArrayTest {
 
   @Test
-  void testMerge() {
+  void testMergeSimple() {
 
     int num1[] = new int[] {1,2,3,0,0,0};
     int num2[] = new int[] {2,5,6};
     MergeSortedArray.merge(num1, 3, num2, 3);
     assertArrayEquals(new int[] {1,2,2,3,5,6}, num1);
+  }
+
+  @Test
+  void testMergeArrayWithSameElements() {
+
+    int num1[] = new int[] {1,1,0,0,0};
+    int num2[] = new int[] {1,1,1};
+    MergeSortedArray.merge(num1, 2, num2, 3);
+    assertArrayEquals(new int[] {1,1,1,1,1}, num1);
+  }
+
+  @Test
+  void testMergeArrayWithOneElement() {
+
+    int num1[] = new int[] {1,0};
+    int num2[] = new int[] {1};
+    MergeSortedArray.merge(num1, 1, num2, 1);
+    assertArrayEquals(new int[] {1,1}, num1);
+  }
+
+  @Test
+  void testMergeEmptyArrays() {
+
+    int num1[] = new int[] {};
+    int num2[] = new int[] {};
+    MergeSortedArray.merge(num1, 0, num2, 0);
+    assertArrayEquals(new int[] {}, num1);
+  }
+
+  @Test
+  void testMergeOneEmptyAndOneNonEmptyArray() {
+
+    int num1[] = new int[] {1,2};
+    int num2[] = new int[] {};
+    MergeSortedArray.merge(num1, 2, num2, 0);
+    assertArrayEquals(new int[] {1,2}, num1);
   }
 }
