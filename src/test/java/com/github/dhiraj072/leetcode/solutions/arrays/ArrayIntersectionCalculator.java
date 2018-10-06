8d88f5af48453caa843d3cc7cd6615926cc86c5d
Dhiraj <dhirajbhatt072@gmail.com>
2018-10-06 20:10:00 +0800

    Solution for Arrray Intersection Calculator - not yet submitted

--- src/test/java/com/github/dhiraj072/leetcode/solutions/arrays/ArrayIntersectionCalculator.java
+++ src/test/java/com/github/dhiraj072/leetcode/solutions/arrays/ArrayIntersectionCalculator.java
@@ -1,5 +1,40 @@
 package com.github.dhiraj072.leetcode.solutions.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayIntersectionCalculator {
 
+  public static List<Integer> intersect(int[] nums1, int[] nums2) {
+
+    List<Integer> result = new ArrayList<>();
+    Arrays.sort(nums1);
+    Arrays.sort(nums2);
+    int i = 0;
+    int j = 0;
+    while (i < nums1.length && j < nums2.length) {
+
+      if (nums1[i] == nums2[j]) {
+
+        result.add(nums1[i]);
+        i++;
+        j++;
+      } else {
+
+        if (i == 0 || nums1[i] == nums1[i - 1]) {
+
+          i++;
+        } else if (j == 0 || nums2[j] == nums2[j - 1]) {
+
+          j++;
+        } else {
+
+          i++;
+          j++;
+        }
+      }
+    }
+    return result;
+  }
 }
--- src/test/java/com/github/dhiraj072/leetcode/solutions/arrays/ArrayIntersectionCalculatorTest.java
+++ src/test/java/com/github/dhiraj072/leetcode/solutions/arrays/ArrayIntersectionCalculatorTest.java
@@ -1,5 +1,50 @@
+package com.github.dhiraj072.leetcode.solutions.arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 class ArrayIntersectionCalculatorTest {
 
+  @Test
+  void testIntersectCommonElementsTogether() {
+
+    List<Integer> result = Arrays.asList(2, 2);
+    assertEquals(result,
+        ArrayIntersectionCalculator.intersect(new int[] {1,2,2,1}, new int[] {2,2}));
+  }
+
+  @Test
+  void testIntersectCommonElementsSegregated() {
+
+    List<Integer> result = Arrays.asList(4, 9);
+    assertEquals(result,
+        ArrayIntersectionCalculator.intersect(new int[] {4,9,5}, new int[] {9,4,9,8,4}));
+  }
+
+  @Test
+  void testIntersectNoCommonElements() {
+
+    List<Integer> result = Arrays.asList();
+    assertEquals(result,
+        ArrayIntersectionCalculator.intersect(new int[] {2,2}, new int[] {3,3}));
+  }
+
+  @Test
+  void testIntersectOneElementCommon() {
+
+    List<Integer> result = Arrays.asList(1);
+    assertEquals(result,
+        ArrayIntersectionCalculator.intersect(new int[] {1,2,3}, new int[] {5,6,1}));
+  }
+
+  @Test
+  void testIntesectEmptyArrays() {
+
+    List<Integer> result = Arrays.asList();
+    assertEquals(result,
+        ArrayIntersectionCalculator.intersect(new int[] {}, new int[] {}));
+  }
 }
